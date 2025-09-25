13ef89ea560788983e279b989591aeb86f816acd
joao <jpgmoreira19@gmail.com>
2025-09-25 19:24:58 -0300

    creating files and folders in root working

--- src/renderer/components/UI/TreeView.vue
+++ src/renderer/components/UI/TreeView.vue
@@ -1,17 +1,28 @@
 <script lang="ts" setup>
-  import { reactive, useTemplateRef } from 'vue';
+  import { computed, reactive, useTemplateRef } from 'vue';
 
   // --- Types and structures: ---
 
   type Node =
     | {
         type: 'dir';
+        text: string;
+        parent: Node | null;
+        next: Node | null;
+        prev: Node | null;
+        head: Node | null;
+        tail: Node | null;
       }
     | {
         type: 'file';
+        text: string;
+        parent: Node | null;
+        next: Node | null;
+        prev: Node | null;
       };
 
   type ContextType = 'root' | 'dir' | 'file';
+  type NodeType = Node['type'];
 
   const context = reactive({
     type: 'root' as ContextType,
@@ -21,9 +32,16 @@
     targetDomElement: null as HTMLInputElement | null,
   });
 
+  const rootController = reactive({
+    nextFile: 1,
+    nextDir: 1,
+    head: null as Node | null,
+    tail: null as Node | null,
+  });
+
   const treeContainerRef = useTemplateRef('tree-container');
 
-  // --- Context menu functions: ---
+  // --- Context menu: ---
 
   function computeContextStyle(x: number, y: number) {
     const treeContainer = treeContainerRef.value;
@@ -34,16 +52,10 @@
     const left = x - rect.left;
     const top = y - rect.top;
     const res = {} as (typeof context)['style'];
-    if (left + MENU_WIDTH > rect.width) {
-      res['right'] = `${rect.width - left}px`;
-    } else {
-      res['left'] = `${left}px`;
-    }
-    if (top + MENU_HEIGHT > rect.height) {
-      res['bottom'] = `${rect.height - top}px`;
-    } else {
-      res['top'] = `${top}px`;
-    }
+    if (left + MENU_WIDTH > rect.width) res['right'] = `${rect.width - left}px`;
+    else res['left'] = `${left}px`;
+    if (top + MENU_HEIGHT > rect.height) res['bottom'] = `${rect.height - top}px`;
+    else res['top'] = `${top}px`;
     context['style'] = res;
   }
 
@@ -53,19 +65,87 @@
     computeContextStyle(e.clientX, e.clientY);
   }
 
-  // --- Computed properties: ---
+  function clearContext() {
+    context.visible = false;
+    context.targetNode = null;
+    context.targetDomElement = null;
+  }
+
+  // --- Node creation: ---
+
+  function _createDirNode(parent: Node | null): Node {
+    const node = {
+      type: 'dir',
+      text: `Folder ${rootController.nextDir}`,
+      parent,
+      next: null,
+      prev: null,
+      head: null,
+      tail: null,
+    } as const;
+    rootController.nextDir++;
+    return node;
+  }
+  function _createFileNode(parent: Node | null): Node {
+    const node = {
+      type: 'file',
+      text: `File ${rootController.nextFile}`,
+      parent,
+      next: null,
+      prev: null,
+    } as const;
+    rootController.nextFile++;
+    return node;
+  }
+
+  function createNode(type: NodeType, parent: Node | null) {
+    if (parent && parent.type !== 'dir') return;
+    const newNode = type === 'dir' ? _createDirNode(parent) : _createFileNode(parent);
+    const control = parent || rootController;
+    if (!control.head || !control.tail) {
+      control.head = newNode;
+      control.tail = newNode;
+    } else {
+      control.tail.next = newNode;
+      newNode.prev = control.tail;
+      control.tail = newNode;
+    }
+  }
+
+  // --- Tree flattening: ---
+
+  function flatten(head: Node | null): Node[] {
+    const result: Node[] = [];
+    let curr: Node | null = head;
+    while (curr) {
+      result.push(curr);
+      if (curr.type === 'dir') result.push(...flatten(curr.head));
+      curr = curr.next;
+    }
+    return result;
+  }
+
+  const flattened = computed(() => flatten(rootController.head));
 </script>
 
 <template>
-  <div class="tree-container" ref="tree-container" @click.right="(e) => showContext('root', e)">
+  <div
+    class="tree-container"
+    ref="tree-container"
+    @click.right="(e) => showContext('root', e)"
+    @click="clearContext"
+  >
     <!-- Context menu -->
     <div v-if="context.visible" class="context-container" :style="context.style">
       <div v-if="context.type === 'root'">
-        <div>Create file</div>
-        <div>Create folder</div>
+        <div @click="createNode('file', null)">Create file</div>
+        <div @click="createNode('dir', null)">Create folder</div>
       </div>
     </div>
-    <!--  -->
+    <!-- Tree -->
+    <div v-for="node in flattened" class="node-row">
+      {{ node.text }}
+    </div>
   </div>
 </template>
 
