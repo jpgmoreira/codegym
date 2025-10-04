091efeaf18321d1a78d9e1ac886976bda962f83d
joao <jpgmoreira19@gmail.com>
2025-10-04 13:02:48 -0300

    new node structure

--- src/common/types/tree/treeTypes.ts
+++ src/common/types/tree/treeTypes.ts
@@ -1,48 +1,14 @@
 /**
  * Common types for the treeview component.
- * The treeview has logic spread in both the main and renderer processes.
+ * The treeview has logic implemented in both the main and renderer processes.
  */
 
-type BaseNode = {
-  id: string;
-  text: string;
-  depth: number;
-  selected: boolean;
-  deleted: boolean;
-  parentId: string | null;
-  // Siblings of the same type in the chain:
-  nextSiblingId: string | null;
-  prevSiblingId: string | null;
-  // Next and previous in the flattened tree:
-  nextFlattenId: string | null;
-  prevFlattenId: string | null;
-};
-
-export type DirNode = BaseNode & {
-  type: 'dir';
-  nDesc: number;
-  nDescSel: number;
-  subDirs: ListPointers;
-  subFiles: ListPointers;
-  open: boolean;
-};
-
-export type FileNode = BaseNode & {
-  type: 'file';
-};
-
-export type Node = DirNode | FileNode;
-
-export type NodeType = Node['type'];
-
-export type ListPointers = {
-  headId: string | null;
-  tailId: string | null;
-};
-
-export type Controller = {
-  subDirs: ListPointers;
-  subFiles: ListPointers;
+type Node = {
+  id: number;
+  txt: string;
+  sel: boolean;
+  par: number;
+  opn: boolean;
 };
 
 export type TreeState = {
