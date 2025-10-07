26a5fa0eac769eee78eb772e1dcd000371e8c3a3
joao <jpgmoreira19@gmail.com>
2025-10-07 12:21:33 -0300

    removed firsthistorypages from common and main

--- src/common/schemas/startup.ts
+++ src/common/schemas/startup.ts
@@ -1,21 +1,10 @@
-import { Oj, OjList } from '@common/types/oj';
 import { OjMeta } from './ojMeta';
 import { Profile, ProfileRegistry } from './profile';
-import { OjProblem } from './problems';
 import { GraphRecord } from './graph';
-
-export type FirstHistoryPages = { [K in Oj]: OjProblem[K][] };
 
 export type StartupData = {
   ojMeta: OjMeta;
   currProfile: Profile | null;
   profileRegistry: ProfileRegistry;
-  firstHistoryPages: FirstHistoryPages;
   graphData: GraphRecord[];
 };
-
-export function getEmptyFirstHistoryPages(): FirstHistoryPages {
-  const result: Partial<FirstHistoryPages> = {};
-  for (const oj of OjList) result[oj] = [];
-  return result as FirstHistoryPages;
-}
--- src/main/data/startup.ts
+++ src/main/data/startup.ts
@@ -3,9 +3,7 @@ import { GraphManager } from './managers/graphManager';
 import { OjMetaManager } from './managers/ojMetaManager';
 import { ProfileManager } from './managers/profileManager';
 import { HistoryManager } from './managers/historyManager';
-import { OjList } from '@common/types/oj';
-import { getEmptyFirstHistoryPages, StartupData } from '@common/schemas/startup';
-import { OjProblem } from '@common/schemas/problems';
+import { StartupData } from '@common/schemas/startup';
 import { CacheManager } from './managers/cacheManager';
 
 export async function loadStartupData(): Promise<StartupData> {
@@ -13,22 +11,16 @@ export async function loadStartupData(): Promise<StartupData> {
   const ojMeta = OjMetaManager.instance.getAllMeta();
   const currProfile = ProfileManager.instance.getCurrProfile();
   const profileRegistry = ProfileManager.instance.getProfileRegistry();
-  let firstHistoryPages = getEmptyFirstHistoryPages();
   let graphData: GraphRecord[] = [];
   if (currProfile) {
     GraphManager.instance.loadGraph(currProfile.id);
     graphData = await GraphManager.instance.getGraphData();
     HistoryManager.instance.loadHistory(currProfile.id);
-    for (const oj of OjList) {
-      const page = await HistoryManager.instance.fetchHistoryPage(oj, 1);
-      (firstHistoryPages[oj] as OjProblem[typeof oj][]) = page;
-    }
   }
   const result = {
     ojMeta,
     currProfile,
     profileRegistry,
-    firstHistoryPages,
     graphData,
   };
   return result;
