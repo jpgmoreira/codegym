f53c238d1c1843d2fa7f32ff9e45e7353bad9b0b
joao <jpgmoreira19@gmail.com>
2025-10-14 19:28:17 -0300

    improved preload.ts

--- src/renderer/preload.ts
+++ src/renderer/preload.ts
@@ -9,21 +9,28 @@ import {
   type ElectronAPI,
 } from '@renderer/api/api';
 
+type SendChannel = (typeof allowedSendChannels)[number];
+type InvokeChannel = (typeof allowedInvokeChannels)[number];
+type OnChannel = (typeof allowedOnChannels)[number];
+
 // https://stackoverflow.com/questions/57807459/how-to-use-preload-js-properly-in-electron
 contextBridge.exposeInMainWorld(
   'api',
   Object.freeze({
-    send: (channel, ...data) => {
-      if (allowedSendChannels.includes(channel)) {
-        ipcRenderer.send(channel, ...data);
+    send: (channel: SendChannel, ...data: unknown[]) => {
+      if (!allowedSendChannels.includes(channel)) {
+        console.warn(`[api.send] Channel "${channel}" is not allowed.`);
+        return;
       }
+      ipcRenderer.send(channel, ...data);
     },
-    invoke: (channel, ...data) => {
-      if (allowedInvokeChannels.includes(channel)) {
-        return ipcRenderer.invoke(channel, ...data);
+    invoke: (channel: InvokeChannel, ...data: unknown[]) => {
+      if (!allowedInvokeChannels.includes(channel)) {
+        return Promise.reject(new Error(`[api.invoke] Channel "${channel}" is not allowed.`));
       }
+      return ipcRenderer.invoke(channel, ...data);
     },
-    on: (channel, func) => {
+    on: (channel: OnChannel, func: (...data: unknown[]) => void) => {
       if (allowedOnChannels.includes(channel)) {
         ipcRenderer.on(channel, (_, ...args) => func(...args));
       }
